6cda7332d9f26595fc33f794faa51f0b13dc908a
JeppeSigaard <jeppe@smatmonkey.dk>
2016-01-14 10:27:42 +0100

    rettelser, analytics mv.

--- template-parts/front-page/front-boxes.php
+++ template-parts/front-page/front-boxes.php
@@ -7,9 +7,9 @@ if($boxes && is_array($boxes)) : ?>
         <div class="front-box">
             <div class="inner">
             <?php if($box['field_link']) : ?>
-            <a href="<?php echo get_the_permalink($box['field_link']); ?>" class="box-icon"><?php include get_attached_file($box['field_icon']); ?></a>
+            <a href="<?php echo get_the_permalink($box['field_link']); ?>" class="box-icon"><?php if (!empty($box['field_icon'])) {include get_attached_file($box['field_icon']);} ?></a>
             <?php else :  ?>
-            <div class="box-icon"><?php include get_attached_file($box['field_icon']); ?></div>
+            <div class="box-icon"><?php if (!empty($box['field_icon'])) {include get_attached_file($box['field_icon']);} ?></div>
             <?php endif; ?>
             <h3 class="box-header"><?php echo wp_strip_all_tags($box['field_heading']); ?></h3>
             <p><?php echo wp_strip_all_tags($box['field_text']); ?></p>
